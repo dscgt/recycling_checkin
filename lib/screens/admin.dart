b45431025cdc05a73decfaf68a25d9778d851a44
Michael Fan <michaelyfan100@gmail.com>
2020-02-15 02:44:54 -0500

    Fix option set no properties bug, move option set creation form to own widget

--- lib/screens/admin.dart
+++ lib/screens/admin.dart
@@ -13,52 +13,11 @@ class Admin extends StatefulWidget {
 }
 
 class AdminState extends State<Admin> {
-  final _optionSetFormKey = GlobalKey<FormState>();
   Future<List<DataCategory>> _optionSetListFuture = getCategories();
   bool showOptionSetForm = false;
-  final _optionSetNameController = TextEditingController();
-  List<DataProperty> optionSetProperties = [];
 
   initState() {
-    /// Initialize the form with an empty option set to give the user a
-    /// starting point.
-    optionSetProperties = [
-      DataProperty(
-        title: '',
-        type: DataType.string
-      )
-    ];
-
     super.initState();
-  }
-
-  _handleAddOptionSet() async {
-    if (!_optionSetFormKey.currentState.validate()) {
-      return;
-    }
-
-    DataCategory toAdd = DataCategory(
-      title: _optionSetNameController.text,
-      properties: optionSetProperties
-    );
-
-    await addDataCategory(toAdd);
-
-    /// After adding an option set, hide the form and refresh the future that
-    /// retrieves option sets.
-    setState(() {
-      showOptionSetForm = false;
-      _optionSetListFuture = getCategories();
-    });
-  }
-
-  _handleAddOptionSetProperty() {
-    setState(() {
-      optionSetProperties.add(DataProperty(
-        title: '',
-        type: DataType.string
-      ));
-    });
   }
 
   _handleShowOptionSetForm() {
@@ -82,9 +41,11 @@ class AdminState extends State<Admin> {
     });
   }
 
-  dispose() {
-    _optionSetNameController.dispose();
-    super.dispose();
+  handleOptionSetAdded() {
+    setState(() {
+      showOptionSetForm = false;
+      _optionSetListFuture = getCategories();
+    });
   }
 
   Widget _buildOptionSetList(List<DataCategory> categories) {
@@ -138,101 +99,8 @@ class AdminState extends State<Admin> {
       );
     }).toList();
 
-    return ListView(
-      shrinkWrap: true,
+    return Column(
       children: categoryElements
-    );
-  }
-
-  Widget _buildOptionSetForm() {
-    List<Widget> formElements = [
-      /// First, create the field where user can define an option set's name.
-      TextFormField(
-        controller: _optionSetNameController,
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'This field must be filled out.';
-          }
-          return null;
-        },
-        decoration: const InputDecoration(
-          hintText: 'A broad check-out category (ex. Vehicle, Fuel Card...)',
-          labelText: 'Option set name',
-        ),
-      ),
-    ];
-    /// Then, create fields for all properties of the option set. User will be
-    /// able to modify a property's name and type (between DataType.string and
-    /// DataType.integer) and delete properties.
-    for (int i = 0; i < optionSetProperties.length; i++) {
-      DataProperty thisProp = optionSetProperties[i];
-      formElements.add(Row(
-        children: <Widget>[
-          Expanded(
-            child: TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'This field must be filled out.';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                hintText: 'A required detail (ex. name)',
-                labelText: 'Option set detail',
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.remove_circle_outline),
-            onPressed: () {
-              setState(() {
-                optionSetProperties.removeAt(i);
-              });
-            },
-          ),
-          Checkbox(
-            onChanged: (bool checked) {
-              setState(() {
-                optionSetProperties[i].type = checked
-                  ? DataType.number
-                  : DataType.string;
-              });
-            },
-            value: thisProp.type == DataType.number
-          ),
-          Text('Only allow numbers')
-        ],
-      ));
-    }
-    /// Finally, create options for adding more properties to this new option
-    /// set, and submission.
-    formElements.addAll([
-      RaisedButton(
-        onPressed: () => _handleAddOptionSetProperty(),
-        child: Text('Add property?')
-      ),
-      Text(
-        'Note: you won\'t need to add properties for check-in and check-out time. These are handled automatically.',
-      ),
-      Row(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () => _handleAddOptionSet(),
-            child: Text('Submit'),
-          ),
-          RaisedButton(
-            onPressed: () => _handleHideOptionSetForm(),
-            child: Text('Cancel'),
-          )
-        ],
-      )
-    ]);
-
-    return Form(
-      key: _optionSetFormKey,
-      child: Column(
-        children: formElements
-      )
     );
   }
 
@@ -260,13 +128,181 @@ class AdminState extends State<Admin> {
             }
           ),
           showOptionSetForm
-            ? null
+            ? RaisedButton(
+              onPressed: () => _handleHideOptionSetForm(),
+              child: Text('Cancel'))
             : RaisedButton(
               onPressed: () => _handleShowOptionSetForm(),
-              child: Text('Add option set?')
-            ),
-          showOptionSetForm ? _buildOptionSetForm() : null,
+              child: Text('Add option set?')),
+          showOptionSetForm ? OptionSet(
+            addOptionSetCallback: handleOptionSetAdded
+          ) : null,
         ].where((Object o) => o != null).toList(),
+      )
+    );
+  }
+}
+
+class OptionSet extends StatefulWidget {
+  final Function addOptionSetCallback;
+
+  const OptionSet({Key key, this.addOptionSetCallback}): super(key: key);
+
+  @override
+  OptionSetState createState() {
+    return OptionSetState();
+  }
+}
+
+class PropertyEntry {
+  final TextEditingController controller;
+  DataType type;
+
+  PropertyEntry({
+    @required this.controller,
+    @required this.type,
+  });
+}
+
+class OptionSetState extends State<OptionSet> {
+  List<PropertyEntry> optionSetPropertyControllers = [];
+  final _optionSetFormKey = GlobalKey<FormState>();
+  final _optionSetNameController = TextEditingController();
+
+
+  initState() {
+    /// Initialize the form with an empty option set to give the user a
+    /// starting point.
+    optionSetPropertyControllers = [
+      PropertyEntry(
+        controller: TextEditingController(),
+        type: DataType.string
+      )
+    ];
+    super.initState();
+  }
+
+  dispose() {
+    _optionSetNameController.dispose();
+    optionSetPropertyControllers.forEach((PropertyEntry pe) {
+      pe.controller.dispose();
+    });
+    super.dispose();
+  }
+
+  _handleAddOptionSet() async {
+    if (!_optionSetFormKey.currentState.validate()) {
+      return;
+    }
+    DataCategory toAdd = DataCategory(
+      title: _optionSetNameController.text,
+      properties: optionSetPropertyControllers.map((PropertyEntry pe) {
+        return DataProperty(
+          title: pe.controller.text,
+          type: pe.type
+        );
+      }).toList()
+    );
+    await addDataCategory(toAdd);
+
+    widget.addOptionSetCallback();
+  }
+
+  _handleAddOptionSetProperty() {
+    setState(() {
+      optionSetPropertyControllers.add(PropertyEntry(
+        controller: TextEditingController(),
+        type: DataType.string
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> formElements = [
+      /// First, create the field where user can define an option set's name.
+      TextFormField(
+        controller: _optionSetNameController,
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'This field must be filled out.';
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          hintText: 'A broad check-out category (ex. Vehicle, Fuel Card...)',
+          labelText: 'Option set name',
+        ),
+      ),
+    ];
+    /// Then, create fields for all properties of the option set. User will be
+    /// able to modify a property's name and type (between DataType.string and
+    /// DataType.integer) and delete properties.
+    for (int i = 0; i < optionSetPropertyControllers.length; i++) {
+      PropertyEntry thisProp = optionSetPropertyControllers[i];
+      formElements.add(Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              controller: thisProp.controller,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'This field must be filled out.';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                hintText: 'A required detail (ex. name)',
+                labelText: 'Option set detail',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.remove_circle_outline),
+            onPressed: () {
+              setState(() {
+                optionSetPropertyControllers.removeAt(i);
+              });
+            },
+          ),
+          Checkbox(
+              onChanged: (bool checked) {
+                setState(() {
+                  optionSetPropertyControllers[i].type = checked
+                    ? DataType.number
+                    : DataType.string;
+                });
+              },
+              value: thisProp.type == DataType.number
+          ),
+          Text('Only allow numbers')
+        ],
+      ));
+    }
+    /// Finally, create options for adding more properties to this new option
+    /// set, and submission.
+    formElements.addAll([
+      RaisedButton(
+          onPressed: () => _handleAddOptionSetProperty(),
+          child: Text('Add property?')
+      ),
+      Text(
+        'Note: you won\'t need to add properties for check-in and check-out time. These are handled automatically.',
+      ),
+      Row(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () => _handleAddOptionSet(),
+            child: Text('Submit'),
+          ),
+        ],
+      )
+    ]);
+
+    return Form(
+      key: _optionSetFormKey,
+      child: Column(
+          children: formElements
       )
     );
   }
