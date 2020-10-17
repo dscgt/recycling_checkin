3e23a9c994e03335849d989e1b757780c4a29755
Michael Fan <michaelyfan100@gmail.com>
2020-10-16 23:49:08 -0400

    Update for web deploy (pre-test)

--- lib/api.dart
+++ lib/api.dart
@@ -1,14 +1,10 @@
 
-import 'dart:async';
-import 'dart:io';
-
 import 'package:recycling_checkin/classes.dart' as Classes;
 import 'package:recycling_checkin/utils.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
-import 'package:sembast/sembast_io.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sembast_web/sembast_web.dart';
 
 final FirebaseFirestore firestore = FirebaseFirestore.instance;
 Database sembastDb;
@@ -24,16 +20,22 @@ final String groupsCollectionName = 'checkin_groups';
 /// Gets the path on local filesystem that will be used to reference local
 /// storage for Sembast.
 Future<Database> getLocalDb() async {
-  if (sembastDb != null) {
-    return sembastDb;
-  } else {
-    Directory directory = await getApplicationDocumentsDirectory();
-    String dbPath = join(directory.path, 'recycling_checkout.db');
-    DatabaseFactory dbFactory = databaseFactoryIo;
-    Database dbToReturn = await dbFactory.openDatabase(dbPath);
-    sembastDb = dbToReturn;
-    return dbToReturn;
-  }
+  var factory = databaseFactoryWeb;
+  var db = await factory.openDatabase('db1');
+  return db;
+
+//  // Add a new record
+//  var key =
+//      await store.add(db, <String, dynamic>{'name': 'Table', 'price': 15});
+//
+//  // Read the record
+//  var value = await store.record(key).get(db);
+//
+//  // Print the value
+//  print(value);
+//
+//  // Close the database
+//  await db.close();
 }
 
 /// Gets models from Cloud Firestore, and use to update local
@@ -109,11 +111,13 @@ Future<Classes.Group> getGroup(String groupId) async {
 /// Gets cached models from local Sembast database. These are the models
 /// retrieved from the last successful retrieval of models from Firestore.
 Future<List<Classes.Model>> getCachedModels() async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
+
   Finder finder = Finder(
       filter: Filter.isNull('thisFieldShouldNotExist')
   );
-  StoreRef store = intMapStoreFactory.store(localDbDataModelsName);
+
   return store.find(localDb, finder: finder).then((List<RecordSnapshot> snapshots) {
     return snapshots.map((RecordSnapshot snap) =>
         Classes.Model.fromMap(snap.value)
@@ -124,11 +128,13 @@ Future<List<Classes.Model>> getCachedModels() async {
 /// Gets cached groups from local Sembast database. These are the groups
 /// retrieved from the last successful retrieval of groups from Firestore.
 Future<List<Classes.Group>> getCachedGroups() async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
+
   Finder finder = Finder(
     filter: Filter.isNull('thisFieldShouldNotExist')
   );
-  StoreRef store = intMapStoreFactory.store(localDbDataGroupsName);
+
   return store.find(localDb, finder: finder).then((List<RecordSnapshot> snapshots) {
     return snapshots.map((RecordSnapshot snap) =>
       Classes.Group.fromMap(snap.value)
@@ -140,8 +146,8 @@ Future<List<Classes.Group>> getCachedGroups() async {
 /// already stored locally will be deleted and replaced completed with
 /// [models].
 Future<void> updateCachedModels(List<Classes.Model> models) async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
-  StoreRef store = intMapStoreFactory.store(localDbDataModelsName);
 
   /// Transform [models] to a format compatible with Sembast.
   List<Map<String, dynamic>> modelsToAdd = models
@@ -163,8 +169,8 @@ Future<void> updateCachedModels(List<Classes.Model> models) async {
 /// already stored locally will be deleted and replaced completed with
 /// [groups].
 Future<void> updateCachedGroups(List<Classes.Group> groups) async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
-  StoreRef store = intMapStoreFactory.store(localDbDataGroupsName);
 
   /// Transform [groups] to a format compatible with Sembast.
   List<Map<String, dynamic>> groupsToAdd = groups
@@ -178,9 +184,9 @@ Future<void> updateCachedGroups(List<Classes.Group> groups) async {
 
 /// Gets records of items currently checked out.
 Future<List<Classes.CheckedOutRecord>> getRecords() async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
   Filter filter = Filter.isNull('checkinTime');
-  StoreRef store = intMapStoreFactory.store(localDbDataRecordsName);
   return store.find(localDb, finder: Finder(
     filter: filter
   )).then((List<RecordSnapshot> snapshots) {
@@ -197,8 +203,8 @@ Future<List<Classes.CheckedOutRecord>> getRecords() async {
 /// if [record] is successfully checked in, and false if [record] is being
 /// queued due to offline write.
 Future<bool> checkin(Classes.CheckedOutRecord record) async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
-  StoreRef store = intMapStoreFactory.store(localDbDataRecordsName);
 
   Map toAdd = record.record.toMap();
   /// Make toAdd compatible with Firebase format
@@ -220,8 +226,8 @@ Future<bool> checkin(Classes.CheckedOutRecord record) async {
 
 /// Saves the record [record] to local storage. For checking out.
 Future<dynamic> checkout(Classes.CheckedOutRecord record) async {
+  StoreRef store = stringMapStoreFactory.store();
   Database localDb = await getLocalDb();
-  StoreRef store = intMapStoreFactory.store(localDbDataRecordsName);
 
   Map<String, dynamic> toAdd = record.toMap();
   // use current time as checkout time if not specified by record
